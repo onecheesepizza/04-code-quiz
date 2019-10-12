93fa4dff9ccd35cd9df4a24cd3cd3da89647bee4
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-10-11 21:46:10 -0700

    updates questions

--- assets/questions.js
+++ assets/questions.js
@@ -10,24 +10,24 @@ var questions = [
         answer: "parentheses"
     },
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        title: "The best food to eat while coding is ____.",
+        choices: ["not this", "wrong answer", "pizza", "pick another"],
+        answer: "pizza"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "Which of the following is related to version control?",
+        choices: ["CSS", "WIFI", "GIT", "SSH"],
+        answer: "git"
     },
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        title: "FALSE===FALSE evaluates to ____.",
+        choices: ["FALSE", "5", "TRUE", "undefined"],
+        answer: "TRUE"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "Code is executed in the browser by ____.",
+        choices: ["Python", "C++", "Javascript", "Ruby"],
+        answer: "Javascript"
     },
 ];
 // sample questions
